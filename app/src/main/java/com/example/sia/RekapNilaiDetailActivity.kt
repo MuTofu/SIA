5ae5ec0f1549650437965b6fd9d337af0820fbbb
MuTofu <57164519+MuTofu@users.noreply.github.com>
2024-12-23 09:24:44 +0700

    first commit

---
+++ app/src/main/java/com/example/sia/RekapNilaiDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RekapNilaiDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rekap_nilai_detail)
+    }
+}
